89e02cdcf6baa80c4ec43fa0a854fd3852c4ee32
Kirill Ivanov <kiivanov@ozon.ru>
2022-12-24 00:40:23 +0300

    fix connection leak

--- internal/repo/pg_rent_repository.go
+++ internal/repo/pg_rent_repository.go
@@ -180,14 +180,15 @@ func (r *pgRentRepository) MarkAsSent(ctx context.Context, req *api.MarkAsSentRe
 	if len(req.Items) == 0 {
 		return nil, errors.New("ids can not be empty")
 	}
+	sql := "update rent_turkey_outbox set is_sent = true, sent_at = NOW(), tg_message_id = $1, tg_message_desc_id = $2 where id = $3"
+	stmt, err := r.db.Prepare(sql)
+
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
 
 	for _, item := range req.Items {
-		sql := "update rent_turkey_outbox set is_sent = true, sent_at = NOW(), tg_message_id = $1, tg_message_desc_id = $2 where id = $3"
-		stmt, err := r.db.Prepare(sql)
-
-		if err != nil {
-			return nil, err
-		}
 
 		_, err = stmt.Query(item.TgMessageId, item.TgMessageDescId, item.Id)
 
